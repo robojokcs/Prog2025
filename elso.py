655800e4c1c0081edd421b91b15fd983f5766daf
Robika <kissrobert895@gmail.com>
2025-09-24 08:37:29 +0200

    UPDATE

--- elso.py
+++ elso.py
@@ -3,20 +3,58 @@ import random
 szamok = []
 
 
-#100 elemu lista 1-999 között
+#100 elemu lista 1-100 között
 
 #szám generálás
 for i in range(100):
     
-    rszam = random.randint(1,999)
+    rszam = random.randint(1,100)
     
     szamok.append(rszam)
-  
+'''
 print(szamok)
-
+'''
 
 jatek_szam=0
 nem_talaltDB=0
 
 
-kitalalando_szam = random.randint(szamok)   
+kitalalando_szam = szamok[random.randint(0,len(szamok))]
+
+tipp=input("Kérek egy tippet 1-100 között: ")
+while (not tipp.isdecimal()):
+    print("Egész számot adj meg")
+    tipp=input("Kérek egy tippet 1-100 között: ")
+tipp=int(tipp)
+    
+    
+    
+    
+if(tipp<kitalalando_szam):
+    print("A tipp kisebb mint a kitalálandó szám")
+    nem_talaltDB+=1 
+if(tipp>kitalalando_szam):  
+    print("A tipp nagyobb mint a kitalálandó szám")
+    nem_talaltDB+=1
+    
+while (tipp!=kitalalando_szam):
+    tipp=input("Kérek egy tippet 1-100 között: ")
+    
+    
+    while (not tipp.isdecimal()):
+        print("Egész számot adj meg")
+        tipp=input("Kérek egy tippet 1-100 között: ")
+    
+    
+    tipp=int(tipp)
+    
+    
+    
+    if(tipp<kitalalando_szam):
+        print("A tipp kisebb mint a kitalálandó szám")
+        
+    if(tipp>kitalalando_szam):
+        print("A tipp nagyobb mint a kitalálandó szám")
+        
+
+print("Gratulálok eltaláltad a számot")
